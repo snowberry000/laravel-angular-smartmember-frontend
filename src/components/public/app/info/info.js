3cf1a989ca72ea21c0dd5154f568cf5eae4fd77a
Mohsin Riaz <mohsin@likastic.com>
2015-12-15 15:43:41 +0500

    fixes

--- src/components/public/app/info/info.js
+++ src/components/public/app/info/info.js
@@ -27,6 +27,48 @@ app.controller('InfoController', function ($scope, $rootScope,$location, $localS
 
 	if( $scope.isLoggedIn() && $scope.salesPage!='preview' )
 	    $state.go('public.app.lessons',{},{location:false});
+	else
+	{
+		Restangular.one('module', 'home').get().then(function(response){
+		    $scope.loading=false;
+		    $modules=response;
+		    $scope.modules = $modules;
+		    $scope.modules = _.reject($scope.modules,function($mod){
+		        return $mod.lessons.length==0;
+		    });
+		    $.each($scope.modules, function (key, data) {
+		        $.each(data.lessons, function (key, data) {
+		            $scope.lesson_count++;
+		            data.showCounter=$scope.lesson_count;
+		            switch(parseInt(data.access_level_type)){
+		                case 1:
+		                    data.access = 'Public';
+		                    break;
+		                case 2:
+		                    data.access = data.access_level !== undefined && data.access_level !== null && data.access_level.name !== undefined ? data.access_level.name : '';
+		                    break;
+		                case 3:
+		                    data.access = 'Members';
+		                    break;
+		                case 4:
+		                    data.access = 'Draft (admin-only)';
+		                    break;
+		            }
+		            if (data.content != undefined && typeof(data.content) == "string")
+		                data.description = $scope.excerpt( data.content );
+		            else
+		                data.description = data.content;
+		        });
+		        data.lessons = _.toArray(data.lessons);
+		    });
+
+		    $rootScope.Modulelessons=[];
+		    for(var i=0;i<$scope.modules.length;i++)
+		    {   
+		        $rootScope.Modulelessons.push.apply( $rootScope.Modulelessons, $filter('orderBy')($scope.modules[i].lessons, 'sort_order') );
+		    }
+		});
+	}
 
 	if($scope.site.show_syllabus_toggle)
 	{
@@ -38,45 +80,7 @@ app.controller('InfoController', function ($scope, $rootScope,$location, $localS
 	{
 	    delete $localStorage.syllabus_format;
 	}
-	/*Restangular.one('module', 'home').get().then(function(response){
-	    $scope.loading=false;
-	    $modules=response;
-	    $scope.modules = $modules;
-	    $scope.modules = _.reject($scope.modules,function($mod){
-	        return $mod.lessons.length==0;
-	    });
-	    $.each($scope.modules, function (key, data) {
-	        $.each(data.lessons, function (key, data) {
-	            $scope.lesson_count++;
-	            data.showCounter=$scope.lesson_count;
-	            switch(parseInt(data.access_level_type)){
-	                case 1:
-	                    data.access = 'Public';
-	                    break;
-	                case 2:
-	                    data.access = data.access_level !== undefined && data.access_level !== null && data.access_level.name !== undefined ? data.access_level.name : '';
-	                    break;
-	                case 3:
-	                    data.access = 'Members';
-	                    break;
-	                case 4:
-	                    data.access = 'Draft (admin-only)';
-	                    break;
-	            }
-	            if (data.content != undefined && typeof(data.content) == "string")
-	                data.description = $scope.excerpt( data.content );
-	            else
-	                data.description = data.content;
-	        });
-	        data.lessons = _.toArray(data.lessons);
-	    });
-
-	    $rootScope.Modulelessons=[];
-	    for(var i=0;i<$scope.modules.length;i++)
-	    {   
-	        $rootScope.Modulelessons.push.apply( $rootScope.Modulelessons, $filter('orderBy')($scope.modules[i].lessons, 'sort_order') );
-	    }
-	});*/
+	
 
 	$scope.cutString = function(s, n){
 		var cut= s.indexOf(' ', n);
