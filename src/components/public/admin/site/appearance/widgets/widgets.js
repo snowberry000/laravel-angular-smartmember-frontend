83d39856d02c845981691ab55ac5c4cfd029827e
Todd Nestor <todd.nestor@gmail.com>
2015-12-22 14:52:44 -0800

    some widgets updates

--- src/components/public/admin/site/appearance/widgets/widgets.js
+++ src/components/public/admin/site/appearance/widgets/widgets.js
@@ -49,11 +49,6 @@ app.controller( "WidgetsController", function( $scope, $rootScope, $state, $http
 			$scope.hiddenAds = [];
 			$scope.divide( $ads );
 
-			console.log( "display ads: " );
-			console.log( $scope.displayAds );
-			console.log( "hidden ads: " );
-			console.log( $scope.hiddenAds );
-
 		} );
 	}
 
@@ -66,115 +61,8 @@ app.controller( "WidgetsController", function( $scope, $rootScope, $state, $http
 			{
 				$scope.displayAds.push( value );
 			}
-			else
-			{
-				$scope.hiddenAds.push( value );
-			}
 		} );
-	}
-
-	$scope.currentPage = 1;
-
-	$scope.loadMore = function()
-	{
-		$scope.disable = true;
-		Restangular.all( 'siteAds' ).getList( { p: ++$scope.currentPage, site_id: $site.id } ).then( function( ads )
-		{
-			$scope.ads = $scope.ads.concat( ads );
-			if( ads.length > 0 )
-			{
-				$scope.disable = false;
-			}
-			$scope.divide( ads );
-		} );
-	}
-	$scope.$watch( 'displayAds|json', function()
-	{
-		setTimeout( $scope.save, 1000 );
-	} );
-	$scope.$watch( 'hiddenAds|json', function()
-	{
-		setTimeout( $scope.save, 1000 );
-	} );
-
-	$scope.save = function()
-	{
-		$scope.postAds = [];
-		$.each( $scope.displayAds, function( key, value )
-		{
-			value.display = true;
-			value.sort_order = key;
-			$scope.postAds.push( value );
-		} );
-		$.each( $scope.hiddenAds, function( key, value )
-		{
-			value.display = false;
-			value.sort_order = key;
-			$scope.postAds.push( value );
-		} );
-
-		console.log( $scope.postAds );
-		$scope.update();
-	}
-
-	$scope.update = function()
-	{
-		Restangular.all( "putAdvertisementsOrder" ).post( { "adds": $scope.postAds } ).then( function( response )
-		{
-		} );
-	}
-
-	$scope.delete = function( $ad )
-	{
-		var modalInstance = $modal.open( {
-			templateUrl: '/templates/modals/deleteConfirm.html',
-			controller: "modalController",
-			scope: $scope,
-			resolve: {
-				id: function()
-				{
-					return $ad.id
-				}
-			}
-		} );
-		modalInstance.result.then( function()
-		{
-			$ad.remove().then( function( response )
-			{
-				for( var i = 0; i < $scope.ads.length; i++ )
-				{
-					if( $scope.ads[ i ].id == $ad.id )
-					{
-						$scope.ads.splice( i, 1 );
-						break;
-					}
-				}
-				toastr.success( "Notification removed!" );
-			} );
-		} )
 	}
 
 	$scope.init();
-
-	$scope.dragControlListeners = {
-		accept: function( sourceItemHandleScope, destSortableScope )
-		{
-			return true;
-		},
-		itemMoved: function( $event )
-		{
-			console.log( "moved" );
-		},//Do what you want},
-		orderChanged: function( $event )
-		{
-			console.log( "orderchange2" );
-			setTimeout( 200, $scope.save )
-		},//Do what you want},
-
-		dragEnd: function( $event )
-		{
-			$( window ).off();
-		},
-		containment: '#board'//optional param.
-	};
 } );
