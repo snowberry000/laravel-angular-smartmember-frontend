a9bbdf442b84937690d16876c652ccaef0091d25
snowberry000 <snowberry912@gmail.com>
2015-12-23 11:53:36 -0600

    Some progress on creating downloads, getting API issues though

--- src/components/public/admin/site/content/livecasts/livecasts.js
+++ src/components/public/admin/site/content/livecasts/livecasts.js
@@ -1,80 +1,92 @@
-var app = angular.module("app");
+var app = angular.module( "app" );
 
-app.config(function($stateProvider){
+app.config( function( $stateProvider )
+{
 	$stateProvider
-		.state("public.admin.site.content.livecasts",{
+		.state( "public.admin.site.content.livecasts", {
 			url: "/livecasts",
 			templateUrl: "/templates/components/public/admin/site/content/livecasts/livecasts.html",
 			controller: "LivecastsController"
-		})
-}); 
+		} )
+} );
 
-app.controller("LivecastsController", function ($scope,$rootScope,$http,Restangular) {
+app.controller( "LivecastsController", function( $scope, $rootScope, $http, Restangular )
+{
 	$scope.template_data = {
-        title: 'LIVECASTS',
-        description: 'Embed live hangout & webinar code or display past recordings',
-        singular: 'livecast',
-        edit_route: 'public.admin.site.content.livecast',
-        api_object: 'livecast'
-    }
-    $scope.site = $site = $rootScope.site;
-    $scope.data = [];
-    $scope.pagination = {current_page: 1};
-    $scope.pagination.total_count = 1;
+		title: 'LIVECASTS',
+		description: 'Embed live hangout & webinar code or display past recordings',
+		singular: 'livecast',
+		edit_route: 'public.admin.site.content.livecast',
+		api_object: 'livecast'
+	}
+	$scope.site = $site = $rootScope.site;
+	$scope.data = [];
+	$scope.pagination = { current_page: 1 };
+	$scope.pagination.total_count = 1;
 
-    $scope.paginate = function(){
+	$scope.paginate = function()
+	{
 
-        if( typeof $scope.data[ $scope.pagination.current_page] != 'object' ) {
+		if( typeof $scope.data[ $scope.pagination.current_page ] != 'object' )
+		{
 
-            $scope.loading = true;
+			$scope.loading = true;
 
-            var $params = {p: $scope.pagination.current_page, site_id: $site.id};
+			var $params = { p: $scope.pagination.current_page, site_id: $site.id };
 
-            if ($scope.query) {
-                $params.q = encodeURIComponent( $scope.query );
-            }
+			if( $scope.query )
+			{
+				$params.q = encodeURIComponent( $scope.query );
+			}
 
-            Restangular.all('').customGET( $scope.template_data.api_object + '?view=admin&p=' + $params.p + '&site_id=' + $params.site_id + ( $scope.query ? '&q=' + $scope.query : '' )).then(function (data) {
-                $scope.loading = false;
-                $scope.pagination.total_count = data.total_count;
-                $scope.data[ $scope.pagination.current_page] = Restangular.restangularizeCollection( null, data.items, $scope.template_data.api_object );
-            });
-        }
-    }
+			Restangular.all( '' ).customGET( $scope.template_data.api_object + '?view=admin&p=' + $params.p + '&site_id=' + $params.site_id + ( $scope.query ? '&q=' + $scope.query : '' ) ).then( function( data )
+			{
+				$scope.loading = false;
+				$scope.pagination.total_count = data.total_count;
+				$scope.data[ $scope.pagination.current_page ] = Restangular.restangularizeCollection( null, data.items, $scope.template_data.api_object );
+			} );
+		}
+	}
 
-    $scope.paginate();
+	$scope.paginate();
 
-    $scope.search = function()
-    {
-        $scope.loading = true;
-        $scope.data = [];
-        $scope.pagination = {current_page: 1};
-        var $params = { site_id :$site.id , p : $scope.pagination.current_page};
+	$scope.search = function()
+	{
+		$scope.loading = true;
+		$scope.data = [];
+		$scope.pagination = { current_page: 1 };
+		var $params = { site_id: $site.id, p: $scope.pagination.current_page };
 
-        if ($scope.query){
-            $params.q = encodeURIComponent( $scope.query );
-        }
+		if( $scope.query )
+		{
+			$params.q = encodeURIComponent( $scope.query );
+		}
 
-        Restangular.all('').customGET( $scope.template_data.api_object + '?view=admin&p=' + $params.p + '&site_id=' + $params.site_id + ( $scope.query ? '&q=' + $scope.query : '' ) ).then(function(data){
-            $scope.pagination.total_count = data.total_count;
+		Restangular.all( '' ).customGET( $scope.template_data.api_object + '?view=admin&p=' + $params.p + '&site_id=' + $params.site_id + ( $scope.query ? '&q=' + $scope.query : '' ) ).then( function( data )
+		{
+			$scope.pagination.total_count = data.total_count;
 
-            $scope.data[ $scope.pagination.current_page ] = Restangular.restangularizeCollection( null, data.items, $scope.template_data.api_object );
+			$scope.data[ $scope.pagination.current_page ] = Restangular.restangularizeCollection( null, data.items, $scope.template_data.api_object );
 
-            $scope.loading = false;
-        } , function(error){
-            $scope.data = [];
-        })
-    }
+			$scope.loading = false;
+		}, function( error )
+		{
+			$scope.data = [];
+		} )
+	}
 
-    $scope.deleteResource = function (id) {
+	$scope.deleteResource = function( id )
+	{
 
-        
-            var itemWithId = _.find($scope.data[ $scope.pagination.current_page ], function (next_item) {
-                return next_item.id == id;
-            });
 
-            itemWithId.remove().then(function () {
-                $scope.data[ $scope.pagination.current_page ] = _.without($scope.data[ $scope.pagination.current_page ], itemWithId);
-            });
-    };
-});
+		var itemWithId = _.find( $scope.data[ $scope.pagination.current_page ], function( next_item )
+		{
+			return next_item.id == id;
+		} );
+
+		itemWithId.remove().then( function()
+		{
+			$scope.data[ $scope.pagination.current_page ] = _.without( $scope.data[ $scope.pagination.current_page ], itemWithId );
+		} );
+	};
+} );
