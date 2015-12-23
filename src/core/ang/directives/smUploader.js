14ed66ffb813553fe627e36923d5c2e68437ad1d
snowberry000 <snowberry912@gmail.com>
2015-12-23 14:32:13 -0600

    Revamped media upload modal

--- src/core/ang/directives/smUploader.js
+++ src/core/ang/directives/smUploader.js
@@ -1,151 +1,180 @@
-app.directive('smUploader', function ($localStorage,  $parse, notify, Restangular , smModal) {
-    return {
-        restrict: 'A',
-        require: 'ngModel',
-        
-        link: function (scope, elem, attr , ctrl) {
-            elem.on('click', function () {
-                var key = attr.smUploader;
-                var post = attr.restangular;
-                var model = attr.ngModel;
-                var awskey = attr.ngAwskey;
-                scope.privacy = attr.privacy ? attr.privacy : false;
-                var hideLink = attr.hidelink ? attr.hidelink : false;
+app.directive( 'smUploader', function( $localStorage, $parse, notify, Restangular, smModal )
+{
+	return {
+		restrict: 'A',
+		require: 'ngModel',
 
-                if (post) {
-                    var rest = Restangular.all(post);
-                }
+		link: function( scope, elem, attr, ctrl )
+		{
+			elem.on( 'click', function()
+			{
+				var key = attr.smUploader;
+				var post = attr.restangular;
+				var model = attr.ngModel;
+				var awskey = attr.ngAwskey;
+				scope.privacy = attr.privacy ? attr.privacy : false;
+				var hideLink = attr.hidelink ? attr.hidelink : false;
 
-                smModal.Show(null , {modal_options : {allowMultiple: true}} , 
-                    {templateUrl : 'templates/modals/newMediaItem.html' , controller : 'modalMediaController'} ,
-                    function(item){
-                        if (key) {
-                            var li = {};
-                            console.log(item)
-                            li[key] = item.file;
-                            if (model) {
+				if( post )
+				{
+					var rest = Restangular.all( post );
+				}
 
-                                var parsed_model = $parse(model);
-                                parsed_model.assign(scope, item.file);
-                                ctrl.$setViewValue(item.file);
-                            }
-                            if ( awskey && item.aws_key !== undefined ) {
+				smModal.Show( null, { modal_options: { allowMultiple: true } },
+					{ templateUrl: 'templates/modals/newMediaItem.html', controller: 'modalMediaController' },
+					function( item )
+					{
+						if( key )
+						{
+							var li = {};
+							console.log( item )
+							li[ key ] = item.file;
+							if( model )
+							{
 
-                                var parsed_awskey = $parse(awskey);
-                                parsed_awskey.assign(scope, item.aws_key);
-                                ctrl.$setViewValue(item.aws_key);
-                            }
-                        }
+								var parsed_model = $parse( model );
+								parsed_model.assign( scope, item.file );
+								ctrl.$setViewValue( item.file );
+							}
+							if( awskey && item.aws_key !== undefined )
+							{
 
-                        if (rest) {
-                            rest.customPOST(li, "save").then(function () {
-                                //("Image is uploaded");
-                            });
-                        }
-                    }
-                    )
+								var parsed_awskey = $parse( awskey );
+								parsed_awskey.assign( scope, item.aws_key );
+								ctrl.$setViewValue( item.aws_key );
+							}
+						}
 
-                /*$(".upload.modal")
-                    .modal('setting',{
-                        onApprove: function(){
-                            alert(attributes.smDelete);
-                            scope.deleteResource(attributes.smDelete)
-                        }
-                    })
-                    .modal('show');*/
+						if( rest )
+						{
+							rest.customPOST( li, "save" ).then( function()
+							{
+								//("Image is uploaded");
+							} );
+						}
+					}
+				)
 
-            })
-            
-        }
-    };
-});
-app.controller('modalMediaController', function ($scope, Upload , close) {
-    console.log('we started up');
-    $scope.loading = false;
-    $scope.cancel = function () {
-        $modalInstance.dismiss();
-    };
-    $scope.ok = function () {
-        $modalInstance.close();
-    };
-    $scope.insert = function () {
-        $modalInstance.close();
-    }
+				/*$(".upload.modal")
+				 .modal('setting',{
+				 onApprove: function(){
+				 alert(attributes.smDelete);
+				 scope.deleteResource(attributes.smDelete)
+				 }
+				 })
+				 .modal('show');*/
 
-    $scope.$watch('files', function () {
-        $scope.upload($scope.files);
-    });
+			} )
 
-    $scope.upload = function (files) {
-        //console.log('we are trying to upload a file', files.length, files );
-        if (files) {
-            console.log(files.name);
-            $scope.loading = true;
-            //for (var i = 0; i < files.length; i++) {
-              var file = files;
+		}
+	};
+} );
+app.controller( 'modalMediaController', function( $scope, Upload, close )
+{
+	console.log( 'we started up' );
+	$scope.loading = false;
+	$scope.cancel = function()
+	{
+		$modalInstance.dismiss();
+	};
+	$scope.ok = function()
+	{
+		$modalInstance.close();
+	};
+	$scope.insert = function()
+	{
+		$modalInstance.close();
+	}
 
-              Upload.upload({
-                  url: $scope.app.apiUrl + '/utility/upload' + ( $scope.privacy ? '?private=' + $scope.privacy : '' ),
-                  file: file
-              })
-              .success(function (data, status, headers, config) {
-                      var returnObject = {};
+	$scope.$watch( 'files', function()
+	{
+		$scope.upload( $scope.files );
+	} );
 
-                      returnObject.file = data.file_name;
+	$scope.upload = function( files )
+	{
+		//console.log('we are trying to upload a file', files.length, files );
+		if( files )
+		{
+			console.log( files.name );
+			$scope.loading = true;
+			//for (var i = 0; i < files.length; i++) {
+			var file = files;
 
-                      if( data.aws_key !== undefined )
-                          returnObject.aws_key = data.aws_key;
+			Upload.upload( {
+					url: $scope.app.apiUrl + '/utility/upload' + ( $scope.privacy ? '?private=' + $scope.privacy : '' ),
+					file: file
+				} )
+				.success( function( data, status, headers, config )
+				{
+					var returnObject = {};
 
-                close( returnObject );
-            }).error(function (data, status, headers, config) {
-                console.log('error status: ' + data);
-            });
-
-                //allow only 1 file to be uploaded
-                //break;
-            //}
-        }
-    };
-
-});
-app.controller('inlineMediaController', function ($scope, Upload) {
-    $scope.loading = false;
-
-    $scope.$watch('files', function () {
-        $scope.upload($scope.files);
-    });
-
-    $scope.upload = function (files) {
-        //console.log('we are trying to upload a file', files.length, files );
-        if (files) {
-            console.log(files.name);
-            $scope.loading = true;
-            //for (var i = 0; i < files.length; i++) {
-            var file = files;
-
-            Upload.upload({
-                url: $scope.app.apiUrl + '/utility/upload' + ( $scope.privacy ? '?private=' + $scope.privacy : '' ),
-                file: file
-            })
-                .success(function (data, status, headers, config) {
-                    $scope.loading = false;
-
-                    var returnObject = {};
-
-                    returnObject.file = data.file_name;
+					returnObject.file = data.file_name;
 
                     if( data.aws_key !== undefined )
+                    {
                         returnObject.aws_key = data.aws_key;
+                    }
+
+                    close( returnObject );
+                } ).error( function( data, status, headers, config )
+			{
+				console.log( 'error status: ' + data );
+			} );
+
+			//allow only 1 file to be uploaded
+			//break;
+			//}
+		}
+	};
+
+} );
+app.controller( 'inlineMediaController', function( $scope, Upload )
+{
+	$scope.loading = false;
+
+	$scope.$watch( 'files', function()
+	{
+		$scope.upload( $scope.files );
+	} );
+
+	$scope.upload = function( files )
+	{
+		//console.log('we are trying to upload a file', files.length, files );
+		if( files )
+		{
+			console.log( files.name );
+			$scope.loading = true;
+			//for (var i = 0; i < files.length; i++) {
+			var file = files;
+
+			Upload.upload( {
+					url: $scope.app.apiUrl + '/utility/upload' + ( $scope.privacy ? '?private=' + $scope.privacy : '' ),
+					file: file
+				} )
+				.success( function( data, status, headers, config )
+				{
+					$scope.loading = false;
+
+					var returnObject = {};
+
+					returnObject.file = data.file_name;
+
+                    if( data.aws_key !== undefined )
+                    {
+                        returnObject.aws_key = data.aws_key;
+                    }
 
                     $scope.$parent.meta_data[ $scope.data_to_update ] = returnObject.file;
-                }).error(function (data, status, headers, config) {
+				} ).error( function( data, status, headers, config )
+			{
 
-                });
+			} );
 
-            //allow only 1 file to be uploaded
-            //break;
-            //}
-        }
-    };
+			//allow only 1 file to be uploaded
+			//break;
+			//}
+		}
+	};
 
-});
+} );
