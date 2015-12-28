0c5304c425c8f130665410db0f0d6ebd30da453d
Jawad Ahmed <jawad@likastic.com>
2015-12-28 10:53:14 +0500

    keydown event added to dropdown

--- src/core/ang/directives/suiDropdown.js
+++ src/core/ang/directives/suiDropdown.js
@@ -4,7 +4,14 @@ app.directive( 'suiDropdown', function()
 		restrict: 'A',
 		link: function( scope, next_item, attributes )
 		{
-			$(next_item).dropdown()
+			$(next_item).dropdown();
+			$(next_item).on('keydown' , function(event){
+				if (event.which == 13){
+					var href = $('.item.selected:first a').attr('href');
+					if(href)
+						window.location.href = href;
+				}
+			})
 		}
 	};
 } );
