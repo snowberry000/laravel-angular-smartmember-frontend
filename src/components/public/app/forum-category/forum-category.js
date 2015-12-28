853a5f756d2e9ea912062eb1dee26e32465dcad9
Mohammad Asif <asif@likastic.com>
2015-12-28 16:07:08 +0500

    IMplemented topics

--- src/components/public/app/forum-category/forum-category.js
+++ src/components/public/app/forum-category/forum-category.js
@@ -6,9 +6,23 @@ app.config(function($stateProvider){
 			url: "/forum-category/:permalink",
 			templateUrl: "/templates/components/public/app/forum-category/forum-category.html",
 			controller: "Forum-categoryController"
-		})
-}); 
+		})}); 
 
-app.controller("Forum-categoryController", function ($scope) {
+
+app.controller("Forum-categoryController", function ($scope,$stateParams,Restangular) {
+	Restangular.one('forumCategory','permalink')
+		.get({permalink: $stateParams.permalink})
+		.then(function(response){
+			$scope.category = response;
+		});
+
+
+	$scope.addTopic = function(title){
+		Restangular.service('forumTopic')
+			.post({title: title, category_id: $scope.category.id})
+			.then(function(resopnse){
+				$scope.category.topics.push(response);
+			});
+	}
 
 });
