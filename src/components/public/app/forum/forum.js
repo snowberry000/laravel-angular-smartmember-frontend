84eabd7d9f6971c681fda8aff9bb87ebddffe004
snowberry000 <snowberry912@gmail.com>
2015-12-28 00:50:41 -0600

    Added empty template for forum; logo now centers and nav hides if no nav items

---
+++ src/components/public/app/forum/forum.js
@@ -0,0 +1,14 @@
+var app = angular.module("app");
+
+app.config(function($stateProvider){
+	$stateProvider
+		.state("public.app.forum",{
+			url: "/forum",
+			templateUrl: "/templates/components/public/app/forum/forum.html",
+			controller: "ForumController"
+		})
+}); 
+
+app.controller("ForumController", function ($scope) {
+
+});
