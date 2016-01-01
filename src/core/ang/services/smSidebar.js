77c3e16b0ff8806694de9c671d55a6e31f5734d5
snowberry000 <snowberry912@gmail.com>
2015-12-31 23:24:46 -0600

    Added responsive nav item on www

--- src/core/ang/services/smSidebar.js
+++ src/core/ang/services/smSidebar.js
@@ -2,18 +2,31 @@ app.factory( 'smSidebar', [ '$rootScope', function( $rootScope )
 {
 	var self = {};
 
-	self.Show = function( the_sidebar, template_file )
+	self.Show = function( the_sidebar, template_file, attributes )
 	{
 		if( the_sidebar == '.left_sidebar_contents' )
 			$rootScope.left_sidebar_contents = 'templates/components/public/sidebars/' + template_file;
 		else if( the_sidebar == '.top_sidebar_contents' )
 			$rootScope.top_sidebar_contents = 'templates/components/public/sidebars/' + template_file;
 
-		$( the_sidebar ? the_sidebar : '.ui.sidebar' ).sidebar( {
+		var the_options = {
 			transition: 'overlay',
 			closable: false,
 			dimPage: false
-		} ).sidebar( 'toggle' );
+		};
+
+
+		if( attributes )
+		{
+			angular.forEach( attributes, function( value, key )
+			{
+				the_options[ key ] = value;
+			} )
+		}
+
+		console.log( 'the attributes', attributes, 'the_options', the_options );
+		
+		$( the_sidebar ? the_sidebar : '.ui.sidebar' ).sidebar( the_options ).sidebar( 'toggle' );
 	};
 
 	self.Close = function( the_sidebar )
