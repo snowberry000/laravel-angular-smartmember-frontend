7e2402afcbced99513a7fbae56d4804dcf4134e8
Mike <mike.shinobi@gmail.com>
2016-01-04 01:25:32 -0600

    Added tests for registration and sm access

---
+++ src/components/public/sign/up/sm_access.e2e.js
@@ -0,0 +1,48 @@
+describe('Test SM invite', function(){
+    it('should register and give people access to SM', function(){
+        function getRandomEmail() {
+            var strValues = "abcdefghijk123456789";
+            var strEmail = "";
+            for (var i = 0; i < strValues.length; i++) {
+                strEmail = strEmail + strValues.charAt(Math.round(strValues.length * Math.random()));
+            }
+            return strEmail + "@mymail.test";
+        };
+
+        function getRandomNumber() {
+            var randomNumber = "";
+            var possible = "0123456789";
+            for (var i = 0; i < 3; i++)
+                randomNumber += possible.charAt(Math.floor(Math.random() * possible.length));
+            return randomNumber;
+        }
+
+        browser.get('http://'+browser.params.subdomain+'.smartmember.'+browser.params.env + 'access/5d99d2de51be1c522546d17f4cab7d30');
+        element( by.css( '.register_button' ) ).click().then( function()
+        {
+            browser.waitForAngular();
+            var email = getRandomEmail();
+            element(by.model('user.first_name')).sendKeys('Mike');
+            element(by.model('user.email')).sendKeys(email);
+            element(by.model('user.password')).sendKeys('minhdeptrai');
+
+            element(by.css('button[type=submit]')).click().then(function(){
+                browser.waitForAngular();
+                expect(browser.getCurrentUrl()).toContain('my.smartmember' + browser.params.env);
+            });
+        });
+    });
+
+    it('should allow people to create site', function() {
+        element(by.css('.create_new_site')).click().then(function() {
+            browser.waitForAngular();
+            var site_number = getRandomNumber();
+            element(by.model('site.name')).sendKeys('Test site');
+            element(by.model('site.subdomain')).sendKeys('testsite' + site_number);
+            element(by.css('.green.button')).click().then(function() {
+                browser.waitForAngular();
+                expect(browser.getCurrentUrl()).toContain('testsite' + site_number + '.smartmember.in');
+            });
+        });
+    });
+});
