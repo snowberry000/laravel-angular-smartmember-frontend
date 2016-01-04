60fbae3c8ec08806402b157684fd43096993892b
Mike <mike.shinobi@gmail.com>
2016-01-03 21:55:07 -0600

    Modified classname and added a test

---
+++ src/components/public/sign/in/in.e2e.js
@@ -0,0 +1,18 @@
+describe( 'Sign people in successfully', function()
+{
+    it( 'should be able to sign people in', function()
+    {
+        browser.get( 'http://' + browser.params.subdomain + '.smartmember.' + browser.params.env );
+        element( by.css( '.login_button' ) ).click().then( function()
+        {
+            browser.waitForAngular();
+            element(by.model('user.email')).sendKeys(browser.params.non_admin_user.email);
+            element(by.model('user.password')).sendKeys(browser.params.non_admin_user.password);
+            element( by.css( 'input[type=submit]' ) ).click().then( function()
+            {
+                browser.waitForAngular();
+                expect(browser.getCurrentUrl()).toContain('/lessons');
+            })
+        } );
+    } );
+} );
