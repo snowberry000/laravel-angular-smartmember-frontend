ce91676b74efaed2b34806072d72fc8212cc8f8d
Jawad Ahmed <jawad@likastic.com>
2016-01-06 17:35:10 +0500

    Integration url fixed

--- src/components/public/administrate/team/app_configuration/choose/choose.js
+++ src/components/public/administrate/team/app_configuration/choose/choose.js
@@ -4,7 +4,7 @@ app.config(function($stateProvider){
 	$stateProvider
 		.state("public.administrate.team.integration.choose",{
 			url: "/choose",
-			templateUrl: "/templates/components/public/administrate/team/integration/choose/choose.html",
+			templateUrl: "/templates/components/public/administrate/team/app_configuration/choose/choose.html",
 			controller: "app_configurationsController"
 		})
 }); 
