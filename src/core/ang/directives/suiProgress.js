4ff6fe4452909382f524a12e27ec7448bff79868
snowberry000 <snowberry912@gmail.com>
2016-01-08 16:28:06 -0600

    Fixed progressbar to work on importing

--- src/core/ang/directives/suiProgress.js
+++ src/core/ang/directives/suiProgress.js
@@ -6,31 +6,51 @@ app.directive( 'suiProgress', function()
 		{
 			var the_options = {
 				text: {
-					success : 'setup completed!'
+					success: 'setup completed!'
 				}
 			};
 
-			if( attributes.text )
-				the_options[ 'text' ] = attributes.text;
+			if( attributes.text == 'member' )
+			{
+				the_options[ 'text' ] = {
+					active: 'Importing: {value} of ' + attributes.total + ' members processed',
+					success: '{total} Members Imported!'
+				};
+			}
 
-			$(next_item).progress( the_options );
+			$( next_item ).progress( the_options );
+
+			console.log( 'progress options', the_options );
+			console.log( 'progress attributes', attributes );
+			if( attributes.starttrigger )
+			{
+				$( next_item ).progress( 'increment', attributes.value );
+			}
+
 			scope.next_item = next_item;
 			/*scope.$watch(attributes.value, function(value){
 
-				if( attributes.value > 0 )
-					$(next_item).progress('increment', value);//( value );
-			});*/
+			 if( attributes.value > 0 )
+			 $(next_item).progress('increment', value);//( value );
+			 });*/
 		},
-		controller : function($scope , $rootScope){
-			$rootScope.$watch('site.wizard_step', function(value){
-                console.log('we got some wizard values: ', value );
-                if( value && $rootScope.site && $rootScope.site.wizard_step ) {
-                    if ($rootScope.site.wizard_step > 0)
-                        $($scope.next_item).progress({value: value});//( value );
-                } else {
-                    $($scope.next_item).progress({value: 0});//( value );
-                }
-			});
+		controller: function( $scope, $rootScope )
+		{
+			$rootScope.$watch( 'site.wizard_step', function( value )
+			{
+				console.log( 'we got some wizard values: ', value );
+				if( value && $rootScope.site && $rootScope.site.wizard_step )
+				{
+					if( $rootScope.site.wizard_step > 0 )
+					{
+						$( $scope.next_item ).progress( { value: value } );
+					}//( value );
+				}
+				else
+				{
+					$( $scope.next_item ).progress( { value: 0 } );//( value );
+				}
+			} );
 		}
 	};
-});
+} );
