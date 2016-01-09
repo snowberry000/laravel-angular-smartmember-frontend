bf354501cb27bdc48daaafdf5e7597afbb9f6a8a
snowberry000 <snowberry912@gmail.com>
2016-01-09 14:45:44 -0600

    Fixed checkbox directive to properly set & change angular values

--- src/core/ang/directives/suiCheckbox.js
+++ src/core/ang/directives/suiCheckbox.js
@@ -2,13 +2,30 @@ app.directive( 'suiCheckbox', [ '$http', '$localStorage', function( $http, $loca
 {
 	return {
 		restrict: 'A',
-		link: function( scope, next_item, attributes )
+		require: '?ngModel',
+		link: function( scope, next_item, attributes, ngModel )
 		{
 			var options = {
-
+				onChecked: function() {
+					ngModel.$setViewValue( true );
+				},
+				onUnchecked: function() {
+					ngModel.$setViewValue( false );
+				},
 			};
 
 			$( next_item ).checkbox( options );
+
+			ngModel.$render = function () {
+
+				if( typeof ngModel.$viewValue !== "undefined" )
+				{
+					if( ngModel.$viewValue )
+						$( next_item ).checkbox( 'check' );
+					else
+						$( next_item ).checkbox( 'uncheck' );
+				}
+			};
 		}
 	};
 } ] );
