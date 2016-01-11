7c5e5bff1b3cf51f36bfd1413bf4e037bd4fad6b
snowberry000 <snowberry912@gmail.com>
2016-01-11 01:34:25 -0600

    Home directory revamped; on to the single site pages!

--- src/components/public/www/home/home.js
+++ src/components/public/www/home/home.js
@@ -1,18 +1,48 @@
-var app = angular.module("app");
+var app = angular.module( "app" );
 
-app.config(function($stateProvider){
+app.config( function( $stateProvider )
+{
 	$stateProvider
-		.state("public.www.home",{
+		.state( "public.www.home", {
 			templateUrl: "/templates/components/public/www/home/home.html",
 			controller: "WwwHomeController"
-		})
-}); 
+		} )
+} );
 
-app.controller("WwwHomeController", function ($scope, Restangular) {
+app.controller( "WwwHomeController", function( $scope, Restangular )
+{
+	$scope.loading_sites = false;
+	$scope.loading_categories = true;
+	$scope.current_category = '';
 
-	Restangular.all('directory').getList().then(function (data) {
-		console.log( 'the data', data );
-		$scope.site_listings = data;
+	Restangular.all( 'directory/category' ).getList().then( function( data )
+	{
+		if( data.length > 0 )
+		{
+			$scope.current_category = data[0];
+		}
+
+		console.log( 'directory_categories', data );
+		$scope.directory_categories = data;
+
+		$scope.loading_categories = false;
+	} );
+
+	$scope.$watch('current_category', function( new_value, old_value )
+	{
+		if( new_value != old_value )
+		{
+			$scope.loading_sites = true;
+
+			Restangular.all( 'directory?category=' + encodeURIComponent( $scope.current_category ) ).getList().then( function( data )
+			{
+				console.log( 'site_listings', data );
+				$scope.site_listings = data;
+
+				$scope.loading_sites = false;
+			} );
+		}
 	});
 
-});
+
+} );
