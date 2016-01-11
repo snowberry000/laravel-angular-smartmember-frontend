04abc9411f6ecb260022091f360f43284854a7c3
snowberry000 <snowberry912@gmail.com>
2016-01-10 23:31:20 -0600

    Second loading state for affiliate contest results

--- src/components/public/app/affiliateContest/affiliateContest.js
+++ src/components/public/app/affiliateContest/affiliateContest.js
@@ -1,40 +1,51 @@
-var app = angular.module("app");
+var app = angular.module( "app" );
 
-app.config(function($stateProvider){
+app.config( function( $stateProvider )
+{
 	$stateProvider
-		.state("public.app.affiliateContest",{
+		.state( "public.app.affiliateContest", {
 			url: "/affiliateContest/:permalink",
 			templateUrl: "/templates/components/public/app/affiliateContest/affiliateContest.html",
 			controller: "AffiliateContestController"
-		})
-}); 
+		} )
+} );
 
-app.controller('AffiliateContestController', function ($scope,$rootScope, $state, $stateParams, Restangular) {
-    //console.log($affiliateContest);
-    $scope.loading=true;
+app.controller( 'AffiliateContestController', function( $scope, $rootScope, $state, $stateParams, Restangular )
+{
+	//console.log($affiliateContest);
+	$scope.loading = true;
 
-    if($stateParams.permalink)
-        Restangular.one('affiliateContestByPermalink' , $stateParams.permalink).get().then(function(response){
-            $affiliateContest=response;
-            $scope.loading=false;
-            $scope.affiliateContest=$affiliateContest;
-            $scope.getLeaderBoard();
-            $rootScope.page_title = $affiliateContest.title ? $affiliateContest.title : null;
-        });
-    else
-    {
-        $scope.loading=false;
-        $affiliateContest = $site.company_id;
-        $scope.getLeaderBoard();
-    }
-        
+	if( $stateParams.permalink )
+	{
+		Restangular.one( 'affiliateContestByPermalink', $stateParams.permalink ).get().then( function( response )
+		{
+			$affiliateContest = response;
+			$scope.loading = false;
+			$scope.affiliateContest = $affiliateContest;
+			$scope.getLeaderBoard();
+			$rootScope.page_title = $affiliateContest.title ? $affiliateContest.title : null;
+		} );
+	}
+	else
+	{
+		$scope.loading = false;
+		$affiliateContest = $site.company_id;
+		$scope.getLeaderBoard();
+	}
 
-    $scope.getLeaderBoard = function()
-    {
-        Restangular.one('affiliateLeaderboard', $affiliateContest.id).get().then(function(response){
-            console.log(response);
-            $scope.leaderboard=response;
-        });
-    }
-    
-});
+
+	$scope.getLeaderBoard = function()
+	{
+		$scope.loading = true;
+
+		Restangular.one( 'affiliateLeaderboard', $affiliateContest.id ).get().then( function( response )
+		{
+			console.log( response );
+			$scope.leaderboard = response;
+
+
+			$scope.loading = false;
+		} );
+	}
+
+} );
