982cd364382c6c5a4ee4890aa69330bc3dfdba89
Mike <mike.shinobi@gmail.com>
2016-01-12 16:25:48 -0600

    Fixed single download

--- src/components/public/app/download/download.js
+++ src/components/public/app/download/download.js
@@ -20,22 +20,24 @@ app.controller('PublicDownloadController', function ($scope, $rootScope ,$localS
             $scope.loading=false;
             $scope.download = $download;
             $rootScope.page_title = $download.title || $rootScope.page_title;
+
+            if ($scope.download.download_link)
+            {
+                Restangular.all('get').customGET('download/'+$scope.download.id).then(function(response){
+                    $window.location.href = $scope.download.download_link;
+                });
+            } else {
+                Restangular.all('get').customGET('download/'+$scope.download.id).then(function(response){
+                    if( ( response.aws_key != undefined && response.aws_key != '' ) || ( response.my_url != undefined && response.my_url != '' ) )
+                        location.href = $scope.app.apiUrl + '/utility/download?' + ( response.aws_key != undefined ? 'aws_key=' + response.aws_key : 'file=' + response.my_url );
+                    else{
+                        toastr.error("Something went wrong, your file cannot be downloaded at this time");
+                    }
+                })
+            }
         });
-        
-        if ($scope.download.download_link)
-        {
-            Restangular.all('get').customGET('download/'+$scope.download.id).then(function(response){
-                $window.location.href = $scope.download.download_link;
-            });
-        } else {
-            Restangular.all('get').customGET('download/'+$scope.download.id).then(function(response){
-                if( ( response.aws_key != undefined && response.aws_key != '' ) || ( response.my_url != undefined && response.my_url != '' ) )
-                    location.href = $scope.app.apiUrl + '/utility/download?' + ( response.aws_key != undefined ? 'aws_key=' + response.aws_key : 'file=' + response.my_url );
-                else{
-                    toastr.error("Something went wrong, your file cannot be downloaded at this time");
-                }
-            })
-        }
+
+
     }
     
 });
