69510325093500523cb5723abd109b36eb1d6a00
Mike <mike.shinobi@gmail.com>
2016-01-12 16:18:02 -0600

    download content on init

--- src/components/public/app/download/download.js
+++ src/components/public/app/download/download.js
@@ -19,14 +19,21 @@ app.controller('PublicDownloadController', function ($scope, $rootScope ,$localS
         $rootScope.page_title = $download.title || $rootScope.page_title;
     });
 
-    $scope.updateStats = function(download){
-        Restangular.all('get').customGET('download/' + download.id).then(function(response){
-            if( ( response.aws_key != undefined && response.aws_key != '' ) || ( response.my_url != undefined && response.my_url != '' ) )
-                location.href = $scope.app.apiUrl + '/utility/download?' + ( response.aws_key != undefined ? 'aws_key=' + response.aws_key : 'file=' + response.my_url );
-            else{
-                toastr.error("Something went wrong, your file cannot be downloaded at this time!");
-            }
-        })
+    $scope.updateStats = function(){
+        if ($scope.download.download_link)
+        {
+            Restangular.all('get').customGET('download/'+$scope.download.id).then(function(response){
+                $window.location.href = $scope.download.download_link;
+            });
+        } else {
+            Restangular.all('get').customGET('download/'+$scope.download.id).then(function(response){
+                if( ( response.aws_key != undefined && response.aws_key != '' ) || ( response.my_url != undefined && response.my_url != '' ) )
+                    location.href = $scope.app.apiUrl + '/utility/download?' + ( response.aws_key != undefined ? 'aws_key=' + response.aws_key : 'file=' + response.my_url );
+                else{
+                    toastr.error("Something went wrong, your file cannot be downloaded at this time");
+                }
+            })
+        }
     }
     
 });
