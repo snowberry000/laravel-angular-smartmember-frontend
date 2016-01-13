57d1fbe4c3d81d78657f4311fd92261471fb6a38
Todd Nestor <todd.nestor@gmail.com>
2016-01-12 17:51:21 -0800

    fixed unsubscribe so it at least does something

--- src/components/public/sign/unsubscribe/unsubscribe.js
+++ src/components/public/sign/unsubscribe/unsubscribe.js
@@ -9,7 +9,7 @@ app.config(function($stateProvider){
 		})
 }); 
 
-app.controller('unsubscribeController', function ($location,notify,$state,$scope, $rootScope, $localStorage, Restangular) {
+app.controller('unsubscribeController', function ($location,notify,$state,$scope, $rootScope, $localStorage, Restangular, smModal) {
 
     if( $localStorage.unsubscribe_parameters ) {
         $rootScope.$_GET = $localStorage.unsubscribe_parameters;
@@ -17,40 +17,29 @@ app.controller('unsubscribeController', function ($location,notify,$state,$scope
     }
 
     $scope.email = $rootScope.$_GET['hash'];
-    $scope.company_id = $rootScope.$_GET['network_id'];
+    $scope.site_id = $rootScope.$_GET['network_id'];
     $scope.job_id = $rootScope.$_GET['job_id'];
     $scope.list_type = $rootScope.$_GET['list_type'];
     $rootScope.is_admin=true;
     $scope.emailLists=[];
     $scope.reason="";
 
-  var $params =  {'hash':$scope.email,  'company_id':$scope.company_id, 'list_type':$scope.list_type};
 
-  Restangular.one('emailSubscriber/getEmailLists').get($params).then(function(response){
-      for(var i=0;i<response.length;i++)
+      $scope.unsubscribe = function()
       {
-        response[i].checked=false;
+        $ids=[];
+        for(var i=0;i<$scope.emailLists.length;i++)
+        {
+          if($scope.emailLists[i].checked)
+            $ids.push({"id":$scope.emailLists[i].id});
+        }
+
+        $params = {"hash":$scope.email, "job_id": $scope.job_id, "reason":$scope.reason,
+                   "site_id":$scope.site_id, "list_type" : $scope.list_type};
+
+        Restangular.one('emailSubscriber').post('unsubscribe',$params).then(function(response){
+            smModal.Show("public.sign.unsubscribed");
+        });
       }
-      $scope.company_name=response.company_name;
-      $scope.emailLists=response.data;
-  });
-
-
-  $scope.unsubscribe = function()
-  { 
-    $ids=[];
-    for(var i=0;i<$scope.emailLists.length;i++)
-    {
-      if($scope.emailLists[i].checked)
-        $ids.push({"id":$scope.emailLists[i].id});
-    }
-    
-    $params = {'ids':$ids, "hash":$scope.email, "job_id": $scope.job_id, "reason":$scope.reason,
-               "company_id":$scope.company_id, "list_type" : $scope.list_type};
-
-    Restangular.one('emailSubscriber').post('unsubscribe',$params).then(function(response){
-      $state.go("public.sign.unsubscribed");
-    });
-  }
 
 });
