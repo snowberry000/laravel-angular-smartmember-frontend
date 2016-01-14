a9c06771c630e0f089b1d88851cb9f079f8d3a25
Mike <mike.shinobi@gmail.com>
2016-01-14 17:21:44 -0600

    Fixed the env variable

--- src/components/public/administrate/team/helpdesk/tickets/tickets.js
+++ src/components/public/administrate/team/helpdesk/tickets/tickets.js
@@ -117,7 +117,7 @@ app.controller( "TicketsController", function( $scope, $location, $localStorage,
 	$scope.showSite = function(site_id)
 	{
 		var site = _.findWhere($scope.available_sites, {id: site_id});
-		return site.domain ? site.domain : site.subdomain + '.smarmember.' + app.env
+		return site.domain ? site.domain : site.subdomain + '.smarmember.' + $rootScope.app.env
 	}
 
 	$scope.$watch( 'type_to_fetch', function( new_value, old_value )
