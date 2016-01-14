521af82a513af14fc1d8f2b5460cf127a083deaa
Todd Nestor <todd.nestor@gmail.com>
2016-01-14 08:57:08 -0800

    filtered out contenteditable attributes in dynamic directive

--- src/core/ang/directives/dynamic.js
+++ src/core/ang/directives/dynamic.js
@@ -1,18 +1,19 @@
 app.directive('dynamic', function ($compile) {
-  return {
-    restrict: 'A',
-    replace: true,
-    link: function (scope, elem, attrs) {
-      scope.$watch(attrs.dynamic, function(html) {
-          if( html )
-            html = html.replace(/(dynamic=[",'])(.*?)([",'])/g, '');
-        var $html = $('<p>').html(html);
-        $html.find('iframe').each(function() {
-          $(this).wrap('<div class="video_container"></div>');
-        });
-        elem.html( $html.html() );
-        $compile( elem.contents() )(scope);
-      });
-    }
-  };
+    return {
+        restrict: 'A',
+        replace: true,
+        link: function (scope, elem, attrs) {
+            scope.$watch(attrs.dynamic, function (html) {
+                if (html)
+                    html = html.replace(/(dynamic=[",'])(.*?)([",'])/g, '');
+                var $html = $('<p>').html(html);
+                $html.find('*').removeAttr('contenteditable');
+                $html.find('iframe').each(function () {
+                    $(this).wrap('<div class="video_container"></div>');
+                });
+                elem.html($html.html());
+                $compile(elem.contents())(scope);
+            });
+        }
+    };
 });
