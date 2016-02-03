fade12dd52a742c2666ee6b53254de96ed7c1e77
mohsin riaz <mohsin@likastic.com>
2016-02-03 17:34:19 +0500

    edit agent deuplaictes removed

--- src/components/public/app/admin/support/ticket/ticket.js
+++ src/components/public/app/admin/support/ticket/ticket.js
@@ -83,6 +83,9 @@ app.controller( "TicketController", function( $scope, $localStorage, $state, $ro
 					} );
 				}
 			} );
+			$scope.agents = _.uniq($scope.agents , 'user_id');
+
+
 
 			var isAgent = _.find( $scope.agents, { 'id': $user.id } ) || _.find( $scope.agents, { 'id': $user.id + '' } );
 			console.log( $scope.agents )
