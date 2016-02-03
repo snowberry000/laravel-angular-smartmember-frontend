ec295f0a90ef426686e9e26c31aba90bbb615e2b
Todd Nestor <todd.nestor@gmail.com>
2016-02-03 15:44:30 -0800

    fixed rootscope missing on one controller

--- src/components/public/app/admin/pages/pages.js
+++ src/components/public/app/admin/pages/pages.js
@@ -14,7 +14,7 @@ app.config(function($stateProvider){
 		})
 }); 
 
-app.controller("PagesController", function ($scope, $state) {
+app.controller("PagesController", function ($scope, $rootScope, $state) {
     if( !$rootScope.site || $rootScope.site.capabilities.indexOf( 'manage_content' ) == -1 )
         $state.go('public.app.site.home');
 });
