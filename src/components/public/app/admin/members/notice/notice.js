ad2218aaf188093ace2993c4efe1f7d50e318d61
raheeliftikhar5 <mraheeliftikhar1994@gmail.com>
2016-02-12 16:02:01 +0500

    notices page layout fixed

--- src/components/public/app/admin/members/notice/notice.js
+++ src/components/public/app/admin/members/notice/notice.js
@@ -11,6 +11,7 @@ app.config(function($stateProvider){
 
 app.controller("NoticeController", function ($scope,$rootScope,$stateParams,$state,smModal, $location ,$localStorage ,  Restangular, toastr, Upload) {
 	
+	$scope.page_title = "Create Site Notice";
 	$scope.init = function(){
 
 		if (!Modernizr.inputtypes.date) {
