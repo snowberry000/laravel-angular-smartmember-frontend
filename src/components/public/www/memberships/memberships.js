1576a64661637d21c007f6ccd7790270fd4c144d
raheeliftikhar5 <mraheeliftikhar1994@gmail.com>
2016-03-19 17:15:48 +0500

    memberships paga data loading fix

--- src/components/public/www/memberships/memberships.js
+++ src/components/public/www/memberships/memberships.js
@@ -30,7 +30,7 @@ app.controller( "WwwMembershipsController", function( $scope, $http, Restangular
 
 	$scope.calculateReviewStats =function() {
 		_.each($scope.sites, function(site){
-			$scope.site_reviews = site.reviews;
+			$scope.site_reviews = site.site.reviews;
 			$scope.avg_rating = 0;
 
 			_.each($scope.site_reviews, function(review){
