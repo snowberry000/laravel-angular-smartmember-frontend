f905aff3abf779ddba93ef9b666cb5fb5d82f4dd
Jawad Ahmed <jawad@likastic.com>
2016-03-19 17:22:32 +0500

    Menubar for categories

--- src/components/public/www/www.js
+++ src/components/public/www/www.js
@@ -16,5 +16,32 @@ app.config(function($stateProvider){
 }); 
 
 app.controller("WwwController", function ($scope,$location) {
+	$scope.membership_sidebar = false;
+	$scope.active_category = false;
+
+	$scope.ToggleMembershipSidebar = function()
+	{
+		$scope.membership_sidebar = !$scope.membership_sidebar;
+	};
+
+	$scope.ShowMembershipSidebar = function()
+	{
+		return $scope.membership_sidebar;
+	};
+
+	$scope.SetActiveCategory = function( slug )
+	{
+		$scope.active_category = slug;
+	};
+
+	$scope.ResetActiveCategory = function()
+	{
+		//$scope.active_category = false;
+	};
+
+	$scope.GetActiveCategory = function()
+	{
+		return $scope.active_category;
+	}
 
 });
