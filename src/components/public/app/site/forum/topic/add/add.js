f27ada6bc1df5fc392ca64582c1e05102b81b021
Jawad Ahmed <jawad@likastic.com>
2016-03-28 15:22:37 +0500

    Empty title name in forum categories

--- src/components/public/app/site/forum/topic/add/add.js
+++ src/components/public/app/site/forum/topic/add/add.js
@@ -9,10 +9,14 @@ app.config(function($stateProvider){
 		})
 }); 
 
-app.controller("AddTopicController", function ($scope,$rootScope, Restangular, $filter) {
+app.controller("AddTopicController", function ($scope,$rootScope, Restangular, $filter , toastr) {
 
 	$scope.save = function()
 	{
+		if(!$scope.topic || !$scope.topic.title){
+			toastr.error("Empty title name not allowed")
+			return;
+		}
 		console.log($rootScope.category);
 
 		$scope.topic.category_id = $rootScope.category.id;
