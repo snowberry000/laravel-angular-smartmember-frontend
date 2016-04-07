c943474971b98a94abe3bceaf47c2d4863be7b4c
snowberry000 <snowberry912@gmail.com>
2016-04-07 13:34:46 -0500

    Remove rejection debug?

--- src/core/ang/services/http.js
+++ src/core/ang/services/http.js
@@ -31,7 +31,7 @@ app.factory( 'httpInterceptor', function( $q, $rootScope, $injector, $location, 
 		'responseError': function( rejection )
 		{
 			var toastr = $injector.get( "toastr" );
-			console.log( rejection )
+			//console.log( rejection )
 			$rootScope.loaded = true;
 			if( rejection.status == 405 )
 			{
